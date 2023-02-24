29a33cb149209dd4ee34964e3efd953957fc1a15
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-24 11:33:18 -0300

    server construction

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+let routesIndex = require('./routes/index');
+let routesUsers = require('./routes/users');
+
+let app = express();
+
+app.use(routesIndex);
+app.use('/user', routesUsers);
+
+app.listen(3000, '127.0.0.1', () => {
+
+    console.log("servidor rodando!");
+
+});
+
