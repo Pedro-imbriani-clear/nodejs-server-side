fa665f6959f71941beb0937ec72189ed59bc051f
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-24 13:07:20 -0300

    adding database

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,40 @@
-
-
+let NeDB = require("nedb"); 
+let db = new NeDB({
+    filename:"user.db",
+    autoload:true
+})
 module.exports = (app)=>{
     app.get('/users', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users:[{
-            name: 'Hcode',
-            email: 'contato@hcode.com.br',
-            id: 1
-            
-        }]
-    }
-    ); 
+        db.find({}).sort({name:1}).exec((err,users)=>{
+            if (err){
+                console.log(`error : ${err}`);
+                res.status(400).json({
+                    error:err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                }
+        )};
+        })
+  ; 
 
 });
 
-app.get('/users/admin', (req, res) => {
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
+app.post('/users', (req, res) => {
+    
+    db.insert(req.body, (err,user)=>{
+        if(err){
+            console.log(`error:${err}`);
+            res.status(400).json({
+                error : err
+            });
+        } else {
+            res.status(200).json(user);
+        }
+    })
     });
 
-});};
+}
